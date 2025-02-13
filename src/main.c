3e76132ec7fbc2736d9e1b82b0175031f9473b3e
goIntoAction <goIntoAction@users.noreply.github.com>
2025-02-13 16:41:35 +0800

    update

--- src/main.c
+++ src/main.c
@@ -31,13 +31,12 @@ void add_port_forward(int ipv6_port, char *ipv4_address, int ipv4_port) {
 }
 
 void *handle_client(void *arg) {
-    int ipv6_sock = *(int *)arg;
+    int ipv6_sock = *((int *)arg);
+    free(arg); // 释放分配的内存
     char buffer[BUFFER_SIZE];
     
-    // Read data from the IPv6 socket
     ssize_t n = read(ipv6_sock, buffer, sizeof(buffer));
     if (n > 0) {
-        // Iterate over all port forwarding rules
         for (int i = 0; i < num_ports; i++) {
             int ipv4_sock = socket(AF_INET, SOCK_STREAM, 0);
             if (ipv4_sock < 0) {
@@ -51,14 +50,12 @@ void *handle_client(void *arg) {
             addr.sin_port = htons(ports[i].ipv4_port);
             inet_pton(AF_INET, ports[i].ipv4_address, &addr.sin_addr);
 
-            // Connect to the IPv4 address and port
             if (connect(ipv4_sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
                 perror("Failed to connect to IPv4 address");
                 close(ipv4_sock);
                 continue;
             }
 
-            // Forward the data
             write(ipv4_sock, buffer, n);
             close(ipv4_sock);
         }
@@ -76,24 +73,22 @@ void read_config() {
 
     char line[256];
     while (fgets(line, sizeof(line), file)) {
-        if (line[0] == '#') continue; // Ignore comment lines
+        if (line[0] == '#' || strlen(line) <= 1) continue; // Ignore comment lines and empty lines
+        
         if (strncmp(line, "config port", 11) == 0) {
             int ipv6_port = 0;
-            char ipv4_address[INET_ADDRSTRLEN];
+            char ipv4_address[INET_ADDRSTRLEN] = "";
             int ipv4_port = 0;
-            // Read port forwarding config
+            
             while (fgets(line, sizeof(line), file)) {
-                if (strncmp(line, "option ipv6_port", 16) == 0) {
-                    sscanf(line, "option ipv6_port '%d'", &ipv6_port);
-                } else if (strncmp(line, "option ipv4_address", 20) == 0) {
-                    sscanf(line, "option ipv4_address '%s'", ipv4_address);
-                } else if (strncmp(line, "option ipv4_port", 16) == 0) {
-                    sscanf(line, "option ipv4_port '%d'", &ipv4_port);
+                if (sscanf(line, "option ipv6_port '%d'", &ipv6_port) == 1 ||
+                    sscanf(line, "option ipv4_address '%s'", ipv4_address) == 1 ||
+                    sscanf(line, "option ipv4_port '%d'", &ipv4_port) == 1) {
+                    // Successfully parsed one of the options
                 } else if (strncmp(line, "config port", 11) == 0) {
                     break; // Moving to next config block
                 }
             }
-            // Call to add port forwarding
             add_port_forward(ipv6_port, ipv4_address, ipv4_port);
         }
     }
@@ -103,14 +98,17 @@ void read_config() {
 int main() {
     read_config();
 
-    // Create IPv6 listening socket
-    int ipv6_sock = socket(AF_INET6, SOCK_STREAM, 0);
-    struct sockaddr_in6 addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin6_family = AF_INET6;
-    addr.sin6_addr = in6addr_any; // Listening on all interfaces
-
     for (int i = 0; i < num_ports; i++) {
+        int ipv6_sock = socket(AF_INET6, SOCK_STREAM, 0);
+        if (ipv6_sock < 0) {
+            perror("Failed to create IPv6 socket");
+            return EXIT_FAILURE;
+        }
+
+        struct sockaddr_in6 addr;
+        memset(&addr, 0, sizeof(addr));
+        addr.sin6_family = AF_INET6;
+        addr.sin6_addr = in6addr_any;
         addr.sin6_port = htons(ports[i].ipv6_port);
 
         if (bind(ipv6_sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
@@ -122,17 +120,28 @@ int main() {
             perror("Failed to listen on IPv6 socket");
             return EXIT_FAILURE;
         }
-    }
 
-    while (1) {
-        int client_sock = accept(ipv6_sock, NULL, NULL);
-        if (client_sock >= 0) {
-            pthread_t thread;
-            pthread_create(&thread, NULL, handle_client, &client_sock);
-            pthread_detach(thread); // Detach the thread to avoid memory leak
+        printf("Listening on IPv6 port %d\n", ports[i].ipv6_port);
+
+        while (1) {
+            int *client_sock = malloc(sizeof(int)); // 分配内存并存储文件描述符
+            *client_sock = accept(ipv6_sock, NULL, NULL);
+            if (*client_sock >= 0) {
+                pthread_t thread;
+                if(pthread_create(&thread, NULL, handle_client, client_sock) != 0){
+                    perror("Failed to create thread");
+                    close(*client_sock);
+                    free(client_sock);
+                } else {
+                    pthread_detach(thread); // Detach the thread to avoid memory leak
+                }
+            } else {
+                free(client_sock); // 如果accept失败，记得释放内存
+            }
         }
+
+        close(ipv6_sock);
     }
 
-    close(ipv6_sock);
     return 0;
 }
